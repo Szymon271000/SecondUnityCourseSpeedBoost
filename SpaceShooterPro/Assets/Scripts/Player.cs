6bdb650c228d84bcafd9a69563e994bb9d33f6d4
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-01-22 15:25:23 +0100

    SpaceShooterPro

---
+++ SpaceShooterPro/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // public or private reference
+    // data type (int, float, bool, string)
+    // every variable has a name
+    // optional value assigned
+    [SerializeField] private float _speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take turrent position = new position (0,0,0);
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal") * _speed * Time.deltaTime; // Input.GetAxis returns float 
+        float verticalInput = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0));
+
+        // if player position on the y is greater than 0
+        // y position = 0
+        // else if position on the y is less than -3.8f
+        // y pos = -3.8f
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        // if player on the x > 11
+        // x pos = -11
+        // else if player on the x is less than -11
+        // x pos = 11
+
+        if (transform.position.x > 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
+
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime); // Vector3.right = new Vector3 (1,0,0) // time.deltatime 1m per second (incorporate real time)
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        //Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        //transform.Translate(direction);
+    }
+}
